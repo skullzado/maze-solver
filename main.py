a999e6d1e72549329df6e0eb6bda978f86a33b4e
Paul Calzado <pafcalzado@gmail.com>
2023-11-29 18:43:57 +0800

    Add call to draw_line method for testing

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from graphics import Window
+from graphics import Line, Point, Window
 
 
 def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(50, 50), Point(100, 100)))
     win.wait_for_close()
 
 
