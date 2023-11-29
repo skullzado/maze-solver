11d3e3295193487ac0ea567b65520f4c9969bf9b
Paul Calzado <pafcalzado@gmail.com>
2023-11-29 18:22:54 +0800

    Update formatting on line 8

--- graphics.py
+++ graphics.py
@@ -5,7 +5,8 @@ class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas = Canvas(self.__root, bg="white",
+                               height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
