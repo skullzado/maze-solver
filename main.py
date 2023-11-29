33b3afe58405acfccc930283c3346a641279f865
Paul Calzado <pafcalzado@gmail.com>
2023-11-29 17:54:55 +0800

    Update main.py to only have main method

--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-from tkinter import Tk, BOTH, Canvas
-
-
-class Window:
-    def __init__(self, width, height) -> None:
-        self.width = width
-        self.height = height
-        self.__root = Tk()
-        self.title = self.__root.title
-        self.canvas = Canvas()
-        self.canvas.pack()
-        self.running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-
-    def wait_for_close(self):
-        while self.running is True:
-            self.redraw()
-
-    def close(self):
-        self.running = False
+from graphics import Window
 
 
 def main():
