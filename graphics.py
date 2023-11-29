be6f5d64bcf52dac58f772560b8820fe800eb637
Paul Calzado <pafcalzado@gmail.com>
2023-11-29 18:39:16 +0800

    Add draw_line method in Window class

--- graphics.py
+++ graphics.py
@@ -21,6 +21,9 @@ class Window:
             self.redraw()
         print("window closed...")
 
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
     def close(self):
         self.__running = False
 
