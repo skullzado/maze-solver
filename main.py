2982e238f7a97b4b9db48dee0c4b721f04b22cac
Paul Calzado <pafcalzado@gmail.com>
2023-11-29 19:09:43 +0800

    Add testing for drawing cells

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from graphics import Line, Point, Window
+from cell import Cell
 
 
 def main():
     win = Window(800, 600)
-    win.draw_line(Line(Point(50, 50), Point(100, 100)))
+
+    c = Cell(win)
+    c.has_left_wall = True
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = True
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = True
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = True
+    c.draw(300, 300, 500, 500)
+
     win.wait_for_close()
 
 
